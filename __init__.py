4c643251c1cba476785e856704fb0d2c1a49ecd0
Johannes J. Möller <johannes.j.moeller@gmail.com>
2023-04-20 21:46:40 +0200

    added steel-data

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .steel_data import *
---
+++ steel_data.py
@@ -0,0 +1,8 @@
+import os
+import sqlite3
+import os
+
+basedir = __file__.rsplit(os.sep,maxsplit=1)[0]
+
+def connect_steel_data():
+    return sqlite3.connect(f"{basedir}{os.sep}steel-data.sql")
