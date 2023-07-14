50b985dff660ef1aa5de750f5e3e13bc797a322e
Moeller, Johannes  ST/SWE-CMWE <moelljha@schaeffler.com>
2023-07-14 16:27:23 +0200

    backend finished, frontend not yet started

--- exercises/backend.py
+++ exercises/backend.py
@@ -5,6 +5,16 @@ import pandas as pd
 # The path to the folder where this file is located, i.e. the cme/exercises folder: 
 folder = os.path.dirname(os.path.abspath(__file__))
 
+scaler = pickle.load(open("scaler.pickle", "rb"))
+model_A = pickle.load(open("model_A.pickle", "rb"))
+model_Rm = pickle.load(open("model_Rm.pickle", "rb"))
+
 def backend(*params, **kwargs): 
     
-    return params, kwargs
+    x_unscaled = list(kwargs.values())
+    x = scaler.transform([x_unscaled])
+
+    pA = model_A.predict(x)[0]
+    pRm = model_Rm.predict(x)[0]
+
+    return {"A (%)": pA, "Rm (MPa)": pRm}
--- exercises/frontend.py
+++ exercises/frontend.py
@@ -1,11 +1,12 @@
 import streamlit as st
-import plotly.express as px
+#import plotly.express as px
 import pandas as pd
 from backend import backend
 
 def frontend():
-
-    params = ["hello", "world"] 
-    kwargs = {"hello": "world"}
-    results = backend(*params, **kwargs)
+    chemical_composition = {'c(Fe) (%)': 91, 'c(C) (%)':1, 'c(N) (%)':1, 'c(Cr) (%)':1, 'c(Ni) (%)':1,
+       'c(Mo) (%)':1, 'c(Al) (%)':1, 'c(Mn) (%)':1, 'c(Cu) (%)':1, 'c(Si) (%)':1}
+    results = backend(**chemical_composition)
     st.write(results)
+
+    rows = [st.columns(5), st.columns(5)]
